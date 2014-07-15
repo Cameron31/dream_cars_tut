ab3864a4b91bf405ca9c7ff2aa7df3800e90ccbc
Richard Cochrane <screamer42@gmail.com>
2014-07-15 14:29:43 +0200

    Initial Commit

---
+++ app/controllers/car_controller.rb
@@ -0,0 +1,30 @@
+class CarController < ApplicationController
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calculator::Application.config.secret_key_base = '4f83e9a92e45f40e93f0361714be16381dd4e989aa3f42b5c913541e292b6abb66b3110c100c5a9f08b3b1a3e3934c9c78fd27a77f9da8a67bc1193cdfc85906'
